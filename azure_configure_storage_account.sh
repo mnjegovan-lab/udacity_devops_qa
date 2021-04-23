cf3fb5ea3cef4f035b978509572c69172b84f544
Marko Njegovan <marko.njegovan@gmail.com>
2021-04-23 11:25:43 +0200

    westeu >> westeurope

--- azure_configure_storage_account.sh
+++ azure_configure_storage_account.sh
@@ -5,7 +5,7 @@ STORAGE_ACCOUNT_NAME="crossfunctionaldata"
 CONTAINER_NAME="tstate"
 
 # Create resource group
-az group create --name $RESOURCE_GROUP_NAME --location westeu
+az group create --name $RESOURCE_GROUP_NAME --location westeurope
 
 # Create storage account
 az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
